27b347e25f5240bb07f1d6094d85361e532901b4
Brokenddosop <lusigupta1234@gmail.com>
2024-08-15 14:14:01 +0530

    Update venom.py

--- venom.py
+++ venom.py
@@ -4,8 +4,8 @@ import logging
 from aiogram import Bot
 import asyncio
 
-API_TOKEN = 'TOKEN'
-ADMIN_ID = 'YOUR ID'
+API_TOKEN = '7372694290:AAGgG2b_Qn1ihDJpIxaH42XZS8JMGQaZWu8'
+ADMIN_ID = '1001627337557'
 MAX_RESTARTS = 5
 RESTART_PERIOD = 60  # Seconds
 
